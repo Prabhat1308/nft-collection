1cd8f496b7b3833bede55f10e8e3e3a962f94313
Prabhat Verma <prabhatverma329@gmail.com>
2022-12-14 23:42:05 +0530

    first commit

---
+++ hardhat/constants/index.js
@@ -0,0 +1,5 @@
+const WHITELIST_CONTRACT_ADDRESS ="0xc784584A00fFa8d84ADf590be853bED82f842d20";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
